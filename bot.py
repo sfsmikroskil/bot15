1e224a4844dbf586ebf7bd20470cd9c2eb145a15
sfsmikroskil <sfsmikroskil@gmail.com>
2026-09-05 10:44:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,14 +63,15 @@ async def main(nama, email, c):
         #await page.screenshot(path=f"{c}_4hall.png")
         # 7. Filter booth
         await page.mouse.click(420, 30)
-        await page.wait_for_timeout(500)
+        await page.wait_for_timeout(1000)
         #await page.screenshot(path=f"{c}_5filter.png")
         await page.keyboard.type("imi")
-        await page.wait_for_timeout(500)
+        await page.wait_for_timeout(1000)
         #await page.screenshot(path=f"{c}_6booth.png")
         await page.keyboard.press("Enter")
-        await page.wait_for_timeout(500)
+        await page.wait_for_timeout(1000)
         await page.mouse.click(420, 30)
+        await page.keyboard.type("imi")
         await page.keyboard.press("Enter")
         # Sudah masuk boot
 
